fd8e06847605d1995286c99a48fce1ebd67733d0
vinicius.oliveira <vinicius.oliveira@sequor.com.br>
2021-06-03 21:35:59 -0300

    init commit

---
+++ Batman/Controllers/GadgetsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace Api2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GadgetsController : ControllerBase
+    {
+
+        private readonly ILogger<GadgetsController> _logger;
+
+        public GadgetsController(ILogger<GadgetsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Batrang" };
+        }
+    }
+}
---
+++ Spiderman/Controllers/PowersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace Api1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PowersController : ControllerBase
+    {
+        private readonly ILogger<PowersController> _logger;
+
+        public PowersController(ILogger<PowersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Web" };
+        }
+    }
+}
